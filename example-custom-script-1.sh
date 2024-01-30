9c4c569076f55a3d9a34d77d8615b4bc09013d41
Krohnya-chan <104037493+Krohnya-chan@users.noreply.github.com>
2024-01-30 22:59:55 +0800

    Update example-custom-script-1.sh

--- example-custom-script-1.sh
+++ example-custom-script-1.sh
@@ -35,4 +35,5 @@
 #patch content
 #EOF
 #git apply example.patch
-
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
